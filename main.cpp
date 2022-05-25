3a50316e168c21ca917cbd3c918e1f802cb4b94c
Kargath <40135358+KargathEx@users.noreply.github.com>
2022-05-25 16:24:31 +0800

    Update main.cpp
    不能注释掉某一TEST来屏蔽其输出,因为文本读入顺序依赖。

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
+
 #include<vector>
 #include<string>
 #include<map>
-#include<algorithm>	
+#include<algorithm> 
 #include <unordered_map>
 #include<iostream>
 #include<bitset>
@@ -20,54 +20,15 @@
 #include "pprint.h"
 #include "lc.h"
 #include"So.h"
-std::ifstream file("in.txt");
 using namespace std;
 using namespace lc;
 
-//multiple test cases
-auto get(ifstream& file)
+
+int main()
 {
+  std::ifstream file("in.txt");
   std::string input1;
   std::getline(file, input1);
-  Tree* t1=new Tree(input1); //change it by need
-  return t1;
+  auto t1 = Vec<int>(input1);
+  cout << Solution().func_name() << endl;
 }
-//按需复制多次
-TEST(TmpAddTest, c1)
-{
-  auto t1 = get(file);
-  t1->print();
-  ASSERT_EQ(1, 2 - 1);
-  EXPECT_TRUE(true);
-}
-//按需复制多次
-TEST(TmpAddTest, c2)
-{
-  auto t1 = get(file);
-  t1->print();
-  ASSERT_EQ(1, 2 - 1);
-  EXPECT_TRUE(true);
-}
-//按需复制多次
-TEST(TmpAddTest, c3)
-{
-  auto t1 = get(file);
-  t1->print();
-  ASSERT_EQ(1, 2 - 1);
-  EXPECT_TRUE(true);
-}
-int main(int argc, char** argv)
-{
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
-//single testcase
-//int main()
-//{
-//  std::ifstream file("in.txt");
-//  std::string input1;
-//  std::getline(file, input1);
-//  auto t1 =Vec<int>(input1);
-//  cout << Solution().func_name() << endl;
-//}
