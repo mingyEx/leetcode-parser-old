eb8a5c419159c8efa5aab1c0c5b7b8a1f78e8f02
Kargath <40135358+KargathEx@users.noreply.github.com>
2022-05-26 16:30:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,4 +31,15 @@ int main()
   std::getline(file, input1);
   auto t1 = Vec<int>(input1);
   cout << Solution().func_name() << endl;
+  
+  //对于模拟类问题
+  vector<int> init{ -1,0,0,1,1,2,2 };
+  Solution obj(init);
+  for (int i = 0; i < t1.size(); ++i)
+  {
+    std::string sa = t1[i];
+    std::vector<int> sb = t2[i];
+    boost::json::value sc = boost::json::value_from(sb);
+    std::cout << call(obj, sa, sc) << std::endl;
+  }
 }
