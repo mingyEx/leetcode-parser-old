72c51883ce4acc5c4f37b494413f2713aaad25fe
Kargath <40135358+KargathEx@users.noreply.github.com>
2023-01-28 23:19:56 +0800

    Update lc.h

--- lc.h
+++ lc.h
@@ -16,6 +16,7 @@ namespace lc
   {
     int val;
     ListNode* next;
+    ListNode() :val(0), next(nullptr) {};
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next_) : val(x), next{ next_ } {}
   };
